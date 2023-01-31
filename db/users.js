abc40aae8990630d01bb2a0eaebf7ab9c7ced980
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-01-31 09:10:55 -0600

    Finished getUserByUsername, getUser, and password hasing.

--- db/users.js
+++ db/users.js
@@ -2,13 +2,14 @@ const client = require("./client");
 const bcrypt = require("bcryptjs");
 require("dotenv").config();
 const SALT_COUNT = 10;
+
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
   try{ 
 
-    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+    const hashedPassword = bcrypt.hashSync(password, SALT_COUNT);
 
     const {rows:[user]} = await client.query(`
       INSERT INTO users(username, password)
@@ -29,6 +30,18 @@ async function createUser({ username, password }) {
 async function getUser({ username, password }) {
 try{
 const user = await getUserByUsername(username);
+const hashedPassword = user.password;
+
+let passwordCheck = await bcrypt.compare(password, hashedPassword)
+
+if(passwordCheck)
+{
+  delete user.password;
+  return user;
+}else{
+  return;
+}
+
 
 }catch(error){
   throw error 
@@ -42,12 +55,12 @@ async function getUserById(userId) {
 async function getUserByUsername(userName) {
 try{
 const {rows:[user]} = await client.query(`
-SELECT username
+SELECT *
 FROM users
 WHERE username = '${userName}'; 
 `)
-console.log(user.username);
-return user.username;
+
+return user;
 }catch(error){
   throw error
 }
