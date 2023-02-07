0fc7518c4b626aa11feb60266e710a1ff3269de5
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-02-06 20:47:37 -0600

    Got the two functions in activities done. The get all activities and get all public routines taht feature that activity.

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const {getAllActivities, getPublicRoutinesByActivity} = require('../db')
 
 // GET /api/activities/:activityId/routines
+router.get('/:activityId/routines', async(req, res, next) =>
+{
+    const activityId =req.params;
+
+    const routines = await getPublicRoutinesByActivity({activityId});
+
+    try{
+        res.send(routines);
+    }catch(error)
+    {
+        next(error);
+    }
+
+})
 
 // GET /api/activities
+router.get('/', async(req, res, next) =>{
+    const activities = await getAllActivities();
+    
+    try{
+
+        res.send(activities);
+
+    }catch(error){
+        next(error);
+    }
+})
 
 // POST /api/activities
 
 // PATCH /api/activities/:activityId
+
 
 module.exports = router;
