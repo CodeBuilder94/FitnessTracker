8ec6c925f987dd9d9b74acbc65dde60ca24e5723
Fernando Cesena <fcesena1@yahoo.com>
2023-02-10 20:10:00 -0800

    completed Patch/api/routine_activities/:routineActivityId

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const {getRoutineActivityById, getRoutineById, destroyRoutineActivity, getUserByUsername} = require('../db')
+const {getRoutineActivityById, getRoutineById, destroyRoutineActivity, getUserByUsername, canEditRoutineActivity, updateRoutineActivity} = require('../db')
 const {JWT_SECRET} = process.env;
 const jwt = require('jsonwebtoken')
 
 // PATCH /api/routine_activities/:routineActivityId
+router.patch('/:routineActivityId', async (req, res, next) =>{
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+
+    const {routineActivityId} = req.params;
+    const {count, duration } = req.body
+    try{
+        const token =auth.slice(prefix.length);
+        const curentUser = jwt.verify(token, JWT_SECRET);
+
+        const canEdit = await canEditRoutineActivity(routineActivityId, curentUser.id)
+        const {routineId} = await getRoutineActivityById(routineActivityId)
+        if(canEdit){
+            const updatedRA = await updateRoutineActivity({id:routineActivityId, count:count, duration:duration})
+            res.send (updatedRA)
+        }else{
+            
+            const {name} = await getRoutineById(routineId);
+            next({
+                name: "Can't edit",
+                message: `User ${curentUser.username} is not allowed to update ${name}`
+            })
+        }
+    }
+    catch(error){
+        next(error)
+    }
+})
 
 // DELETE /api/routine_activities/:routineActivityId
 router.delete('/:routineActivityId', async (req, res, next) =>{
