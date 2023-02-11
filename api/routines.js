92c883118941df6f48853c23e3d616295025255e
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-02-11 11:08:12 -0600

    Finished Post for /routines

--- api/routines.js
+++ api/routines.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getAllRoutines} = require('../db')
+const {getAllRoutines, createRoutine} = require('../db')
+const {JWT_SECRET} = process.env;
+const jwt = require('jsonwebtoken')
+
 // GET /api/routines
 router.get('/', async (req, res, next) =>{
     const routines = await getAllRoutines();
@@ -8,7 +11,42 @@ router.get('/', async (req, res, next) =>{
     res.send(routines);
 })
 // POST /api/routines
+router.post('/', async (req, res, next) =>{
+    
+    try{
+        const prefix = 'Bearer ';
+        const auth = req.header('Authorization');
+        
+        if(auth)
+        {
+            const token = auth.slice(prefix.length);
+   
+            const curentUser = jwt.verify(token, JWT_SECRET);
+    
+            const {isPublic, name, goal} = req.body;
+            
+            const newRoutine = await createRoutine({creatorId:curentUser.id, isPublic:isPublic, name:name, goal:goal});
 
+            res.send(newRoutine);
+        }
+        else{
+            next({
+                name:"NeedLogin",
+                message:"You must be logged in to perform this action"
+            })
+        }
+
+        
+      
+            
+            
+        
+
+    }catch(error)
+    {
+        next(error);
+    }
+})
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
